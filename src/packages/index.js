b4897a6b1377f040eb78f4e22df82ba4bd9cf5f1
Emil Yangirov <emil.yangirov@list.ru>
2020-06-12 15:02:38 +0500

    fix(build): Add install function and update example

--- src/packages/index.js
+++ src/packages/index.js
@@ -1,7 +1,19 @@
 import '../scss/style.scss';
+import '../directives';
 
 import ButtonAction from './ButtonAction/ButtonAction';
 import ButtonDelete from './ButtonDelete/ButtonDelete';
 import ElGrid from './ElGrid/main.vue';
 
-export { ButtonAction, ButtonDelete, ElGrid };
+const install = function(Vue, opts = {}) {
+  [ButtonAction, ButtonAction, ElGrid].map(component => {
+    Vue.component(component.name, component);
+  });
+};
+
+export default {
+  install,
+  ButtonAction,
+  ButtonDelete,
+  ElGrid
+};
